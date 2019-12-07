678c2941c48095caba8f2c33b48aa1c3e924e2a6
L3n1ad <danieltoth0910@gmail.com>
2019-12-07 12:43:18 +0000

    index route added

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -4,6 +4,18 @@ const ObjectID = require('mongodb').ObjectID;
 const createRouter = function(collection) {
 
   const router = express.Router();
+  // INDEX
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then(docs => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({status:500, error: err})
+      });
+    });
   // show
   router.get('/:id', (req, res) => {
     const id = req.params.id;
