98778d713061b243d73a4e79e3f03c4eb7fa3e32
L3n1ad <danieltoth0910@gmail.com>
2019-12-07 14:18:56 +0000

    taking heroes id to and add it to players hand as default for home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,15 +21,18 @@ export default {
     .then(() => this.allHeroes.map(obj => {
       return obj._id
     }))
-    .then( ids => this.allHeroesID = ids);
-
-    GameService.getAllPlayers()
-      .then( data => {
-        console.log(data);
-        this.playerOne = data[0]
-        this.playerTwo = data[1]
-      })
-
+    .then( ids => this.allHeroesID = ids)
+    .then(() =>{
+      GameService.getAllPlayers()
+        .then( data => {
+          this.playerOne = data[0]
+          this.playerTwo = data[1]
+        })
+        .then(() => {
+          this.playerOne.hand = [this.allHeroesID[5]]
+          this.playerTwo.hand = [this.allHeroesID[27]]
+        })
+    });
   }
 }
 </script>
