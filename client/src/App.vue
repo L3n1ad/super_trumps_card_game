9148070d0b047aa1cbc422a3627c8a55571c001a
Marcin <marcinszydelko@yahoo.com>
2019-12-08 18:50:06 +0000

    added functions scorePlayerOne/Two

--- client/src/App.vue
+++ client/src/App.vue
@@ -57,6 +57,18 @@ export default {
       GameService.updateData(this.playerTwo)
         .then(dbDetailsTwo => this.playerTwo = dbDetailsTwo)
     }
+  },
+  computed: {
+    scorePlayerOne() {
+      if(this.playerOne.hand){
+        return this.playerOne.hand.length;
+      }
+    },
+    scorePlayerTwo() {
+      if(this.playerTwo.hand){
+        return this.playerTwo.hand.length;
+      }
+    }
   }
 }
 </script>
