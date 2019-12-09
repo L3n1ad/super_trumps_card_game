cc77a65607ee94aa2d47c2db32d03604ffb017f3
L3n1ad <danieltoth0910@gmail.com>
2019-12-09 12:20:57 +0000

    add loop for attributes and loading data on the eventBus

--- client/src/components/playerCard.vue
+++ client/src/components/playerCard.vue
@@ -1,11 +1,36 @@
 <template lang="html">
-  <h1 v-if='hero'>{{hero.name}}</h1>
+  <div v-if='hero' class="">
+    <h3>{{hero.name}}</h3>
+    <img v-bind:src="hero.image.url" alt="" width="100">
+    <div class="">
+      <p>Intelligence</p>
+      <p>Strength</p>
+      <p>Speed</p>
+      <p>Durability</p>
+      <p>Power</p>
+      <p>Combat</p>
+    </div>
+    <div class="">
+      <p v-for='(value, attribute) in hero.powerstats' v-on:click='chooseAttribute(attribute, value)'>{{value}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import {eventBus} from '../main.js'
 export default {
   name: "player-card",
-  props: ['hero']
+  props: ['hero'],
+  data(){
+    return{
+      attribute: ''
+    }
+  },
+  methods:{
+    chooseAttribute(attribute, value){
+      eventBus.$emit('chosenAttribute', attribute, value)
+    }
+  }
 }
 </script>
 
