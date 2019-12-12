62964bc025633e420abbf7706632a47ce04e2dfe
matapaska <marta.pasikowska@gmail.com>
2019-12-12 13:40:22 +0000

    improves form

--- client/src/components/newGameForm.vue
+++ client/src/components/newGameForm.vue
@@ -1,30 +1,40 @@
 <template>
   <form class="form-contents" v-on:submit.prevent="handleSubmit">
-    <label for="player1">Player 1:</label>
-    <input type="text" id="player1" placeholder="Name" v-model="player1" required/>
-    <label for="player2">Player 2:</label>
-    <input type="text" id="player2" placeholder="Name" v-model="player2" required/>
-    <br>
-    <label for="noOfCards">Select number of cards:</label>
-    <br>
-    <input type="radio" name="cardAmount" value="30"  v-model="cardAmount" id="noOfCards">30 cards<br>
-    <input type="radio" name="cardAmount" value="16" v-model="cardAmount">16 cards<br>
-    <input type="radio" name="cardAmount" value="6" v-model="cardAmount">6 cards
-    <p>OR</p>
-    <label for="gameTime">Select duration of game:</label><br>
-    <input type="radio" name="gameTime" value="0" v-model="gameTime" id="gameTime">No time<br>
-    <input type="radio" name="gameTime" value="1" v-model="gameTime" >1 min<br>
-    <input type="radio" name="gameTime" value="5" v-model="gameTime">5 min
-    <br>
-    <label for="gameSpeed">Choose game speed:</label><br>
-    <input type="radio" name="gameSpeed" value="0" v-model="gameSpeed" id="gameSpeed">NO TIMER<br>
-    <input type="radio" name="gameSpeed" value="20" v-model="gameSpeed">SLOW<br>
-    <input type="radio" name="gameSpeed" value="5" v-model="gameSpeed">FAST<br>
+    <div class="players-wrapper">
+      <label for="player1" class="label-player">Player 1:</label>
+      <input type="text" id="player1" placeholder="Name" v-model="player1" required/>
+      <label for="player2" class="label-player">Player 2:</label>
+      <input type="text" id="player2" placeholder="Name" v-model="player2" required/>
+      <br>
+    </div>
+    <div class="option-wrapper">
 
+      <div class="option-container">
+        <label for="noOfCards" class="label-option">Select number of cards:</label><br>
+        <input class="input-option" type="radio" name="cardAmount" value="30"  v-model="cardAmount" id="noOfCards"><p class="input-option">30 cards</p><br>
+        <input class="input-option" type="radio" name="cardAmount" value="16" v-model="cardAmount"><p class="input-option">16 cards</p><br>
+        <input class="input-option" type="radio" name="cardAmount" value="6" v-model="cardAmount"><p class="input-option">6 cards</p>
+      </div>
+      <div class="option-container">
+        <label for="gameTime" class="label-option">Select duration of game:</label><br>
+        <input class="input-option" type="radio" name="gameTime" value="0" v-model="gameTime" id="gameTime"><p>No time</p><br>
+        <input class="input-option" type="radio" name="gameTime" value="1" v-model="gameTime"><p>1 min</p><br>
+        <input class="input-option" type="radio" name="gameTime" value="5" v-model="gameTime"><p>5 min</p>
+      </div>
+      <div class="option-container">
+        <label for="gameSpeed" class="label-option">Choose game speed:</label><br>
+        <input class="input-option" type="radio" name="gameSpeed" value="0" v-model="gameSpeed" id="gameSpeed"><p>No timer</p><br>
+        <input class="input-option" type="radio" name="gameSpeed" value="20" v-model="gameSpeed"><p>SLOW</p><br>
+        <input class="input-option" type="radio" name="gameSpeed" value="5" v-model="gameSpeed"><p>FAST</p><br>
+      </div>
+
+    </div>
 
     <br>
-    <input type="submit" name="submit" value="START" />
-    <button v-on:click="closeWindow" name="close-window">Close Form</button>
+    <div class="start-button-wrapper">
+      <input class="start-button" type="submit" name="submit" value="START" />
+    </div>
+    <button v-on:click="closeWindow" name="close-window" class="button-close">X</button>
   </form>
 </template>
 
@@ -63,7 +73,82 @@ export default {
 
 <style lang="css" scoped>
   .form-contents {
-    text-align: center;
+    padding-top: 60px;
     font-size: 1rem;
+  }
+  .option-wrapper{
+    padding-top: 40px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .option-container{
+    /* display: flex;
+    flex-direction: column;
+    justify-content: center; */
+  }
+  .option-container input{
+    float: left;
+    margin-left: 20px;
+    /* text-align: left; */
+
+  }
+  .option-container p{
+    display: inline-block;
+    text-align: left;
+    margin: 0;
+    float: left;
+    padding-left: 25px;
+  }
+  .label-player{
+    margin: 0 10px 0 60px;
+  }
+  .label-option{
+
+  }
+  .input-option{
+
+  }
+
+  .players-wrapper{
+    text-align: center;
+
+  }
+
+  .start-button{
+    font-size: 100%;
+    font-family: inherit;
+    border: 0;
+    padding: 0;
+    color: white;
+    text-shadow: -1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 2px 2px 0 black;
+    font-size: 3em;
+    text-align: center;
+    cursor: pointer;
+    padding-right: 2px;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+    text-align: center;
+  }
+  .start-button-wrapper{
+    text-align: center;
+    color:white;
+  }
+
+  .button-close{
+    position: absolute;
+    left:99%;
+    top:-1%;
+    background-color: red;
+    border:0;
+    border-radius: 50%;
+    width:40px;
+    height:40px;
+    padding:0;
+    margin:0;
+    font-weight: bold;
+    cursor: pointer;
   }
 </style>
